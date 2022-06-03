8248c6fb75f6edaf64be972cc9d085150b1034f6
parriehunter <parriehunter@hotmail.com>
2022-06-03 13:04:36 +1200

    trying to get teh images in index.html to rotate

---
+++ web/js/carouselImage.js
@@ -0,0 +1,16 @@
+/*https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
+carousel for imageslideshow*/
+var myIndexImage = 0;
+carouselImage();
+
+function carouselImage() {
+  var i;
+  var x = document.getElementsByClassName("mainImage");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndexImage++;
+  if (myIndexImage > x.length) {myIndexImage = 1}    
+  x[myIndexImage-1].style.display = "block";  
+  setTimeout(carouselImage, 10000); // Change image every 10 seconds
+}
