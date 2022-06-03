45a526029771217f6775366d3b9ea92c8aed9a1f
parriehunter <parriehunter@hotmail.com>
2022-06-03 13:22:19 +1200

    hmm still trying to get images to scroll

--- web/js/carouselImage.js
+++ web/js/carouselImage.js
@@ -1,16 +1,16 @@
 /*https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
 carousel for imageslideshow*/
 var myIndexImage = 0;
-carouselImage();
+carousel();
 
-function carouselImage() {
+function carousel() {
   var i;
-  var x = document.getElementsByClassName("mainImage");
+  var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   myIndexImage++;
   if (myIndexImage > x.length) {myIndexImage = 1}    
   x[myIndexImage-1].style.display = "block";  
-  setTimeout(carouselImage, 10000); // Change image every 10 seconds
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
