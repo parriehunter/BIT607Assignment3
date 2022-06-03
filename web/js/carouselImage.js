b9cbe229e28467956ad1d21ad4c10f9db8338a7e
parriehunter <parriehunter@hotmail.com>
2022-06-03 13:23:35 +1200

    corrected class name in caroseulImage

--- web/js/carouselImage.js
+++ web/js/carouselImage.js
@@ -5,7 +5,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("mainImage");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
